091110de800e1c099fea37aae76084e163591129
kubakrakowiak <kubamega999kk@onet.pl>
2019-05-11 03:09:07 +0200

    added auth system

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <h1>Contact Us</h1>
+
+    <p>Maets</p>
+    <p>111-111-1111</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::view('contact','contact');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
